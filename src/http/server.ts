8d0d5ccb0bdfa364b8961b96259f0af1e8389b0a
Lucas Ferreira <lucas.simoes.ferreira@gmail.com>
2024-02-06 10:01:14 -0300

    feat: add get and create routes

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,31 +1,11 @@
 import fastify from "fastify";
-import { PrismaClient } from "@prisma/client";
-import { z } from "zod";
+import { createPoll } from "../routes/create-poll";
+import { getPoll } from "../routes/get-poll";
 
 const app = fastify();
 
-const prisma = new PrismaClient();
-
-//GET POST PUT DELETE PATCH HEAD OPTIONS
-app.post("/polls", async (request, reply) => {
-  const createPollBody = z.object({
-    title: z.string(),
-  });
-
-  const { title } = createPollBody.parse(request.body);
-
-  const poll = await prisma.poll.create({
-    data: {
-      title,
-    },
-  });
-
-  return reply.status(201).send({ pollId: poll.id });
-});
-
-app.get("/hello", () => {
-  return "Hello World";
-});
+app.register(createPoll);
+app.register(getPoll);
 
 app.listen({ port: 3333 }).then(() => {
   console.log("Server is running on port 3333");
