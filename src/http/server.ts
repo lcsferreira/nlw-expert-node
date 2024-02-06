f4698efac600997d08f09eb61a2cd9a2e79f58d7
Lucas Ferreira <lucas.simoes.ferreira@gmail.com>
2024-02-06 09:26:27 -0300

    feat: add post poll route

---
+++ src/http/server.ts
@@ -0,0 +1,35 @@
+import fastify from "fastify";
+import { PrismaClient } from "@prisma/client";
+import { z } from "zod";
+
+const app = fastify();
+
+const prisma = new PrismaClient();
+
+//GET POST PUT DELETE PATCH HEAD OPTIONS
+app.post("/polls", async (request, reply) => {
+  const createPollBody = z.object({
+    title: z.string(),
+  });
+
+  const { title } = createPollBody.parse(request.body);
+
+  const poll = await prisma.poll.create({
+    data: {
+      title,
+    },
+  });
+
+  return reply.status(201).send({ pollId: poll.id });
+});
+
+app.get("/hello", () => {
+  return "Hello World";
+});
+
+app.listen({ port: 3333 }).then(() => {
+  console.log("Server is running on port 3333");
+});
+
+//Driver nativo - postgresjs
+// ORMs - TypeORM, Prisma, Sequelize
